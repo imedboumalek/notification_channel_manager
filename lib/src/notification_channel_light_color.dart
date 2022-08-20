78ac8a78cf2aea3cdae780fff7bc9e4132023514
Imed Boumalek <imedboumalek@gmail.com>
2022-08-20 17:30:50 +0100

    added notification light color

--- lib/src/notification_channel_light_color.dart
+++ lib/src/notification_channel_light_color.dart
@@ -1,28 +1,43 @@
 enum LightColor {
+  black,
+  dkGray,
+  gray,
+  ltGray,
+  white,
   red,
-  orange,
-  yellow,
   green,
   blue,
-  purple,
-  white;
+  yellow,
+  cyan,
+  magenta,
+  transparent;
 
   int nativeValue() {
     switch (this) {
-      case LightColor.red:
-        return 0;
-      case LightColor.orange:
-        return 1;
-      case LightColor.yellow:
-        return 2;
-      case LightColor.green:
-        return 3;
-      case LightColor.blue:
-        return 4;
-      case LightColor.purple:
-        return 5;
+      case LightColor.black:
+        return 0xFF000000;
+      case LightColor.dkGray:
+        return 0xFF444444;
+      case LightColor.gray:
+        return 0xFF888888;
+      case LightColor.ltGray:
+        return 0xFFCCCCCC;
       case LightColor.white:
-        return 6;
+        return 0xFFFFFFFF;
+      case LightColor.red:
+        return 0xFFFF0000;
+      case LightColor.green:
+        return 0xFF00FF00;
+      case LightColor.blue:
+        return 0xFF0000FF;
+      case LightColor.yellow:
+        return 0xFFFFFF00;
+      case LightColor.cyan:
+        return 0xFF00FFFF;
+      case LightColor.magenta:
+        return 0xFFFF00FF;
+      case LightColor.transparent:
+        return 0;
     }
   }
 }
